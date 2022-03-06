1143f89feaee2424346e5442d7de7f396c2669e8
GloriaSekyere <gloriasekyere15@gmail.com>
2022-03-06 15:56:11 +0000

    implement fetch data onload and onclick of dice

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import mobileDivider from './images/pattern-divider-mobile.svg';
 import dice from './images/icon-dice.svg';
+import { useEffect, useState, useCallback } from 'react';
 
+const api = "https://api.adviceslip.com/advice";
 
 const App = () => {
+
+  const [advice, setAdvice] = useState(null)
+
+  const fetchAdvice = useCallback(async () => {
+    try {
+      let response = await fetch(api)
+      let data =  await response.json()
+      setAdvice(data.slip.advice)
+    } catch (err) {
+        throw new Error(err.message)
+    }
+  })
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
+
+  const handleClick = () => {
+    fetchAdvice()
+  }
+
   return (
     <main className="main">
+
       <h1>ADVICE #117</h1>
-      <p>"It is easy to sit up and take notice,
-        what's difficult is getting up and taking
-        action."
+
+      <p>"{advice && advice}"
       </p>
+
       <div className="mobile-divider">
         <img src={mobileDivider} alt="dice" />
       </div>
-      <div className="dice">
+
+      <div className="dice" onClick={handleClick}>
         <img src={dice} alt="dice" />
       </div>
+
     </main>
   );
 }
